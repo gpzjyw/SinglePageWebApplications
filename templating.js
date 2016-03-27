e89f0c75a16baf0eaf80f94edddaee07711fd49f
JIPENG <gpzjyw@163.com>
2016-03-27 19:20:43 +0800

    a model template

---
+++ templating.js
@@ -0,0 +1,35 @@
+/*
+ * templating.js
+ * 模块模板文件
+ */
+
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+
+/*global $, templating*/
+
+templating = (function () {
+
+	//---------------- BEGIN MODULE SCOPE VARIABLES --------------
+	//----------------- END MODULE SCOPE VARIABLES ---------------
+
+	//------------------- BEGIN UTILITY METHODS ------------------
+	//-------------------- END UTILITY METHODS -------------------
+
+	//--------------------- BEGIN DOM METHODS --------------------
+	//---------------------- END DOM METHODS ---------------------
+
+	//------------------- BEGIN EVENT HANDLERS -------------------
+	//-------------------- END EVENT HANDLERS --------------------
+
+	//------------------- BEGIN PUBLIC METHODS -------------------
+	//------------------- END PUBLIC METHODS ---------------------
+
+
+	//------------------- RETURN PUBLIC METHODS ------------------
+
+}());
